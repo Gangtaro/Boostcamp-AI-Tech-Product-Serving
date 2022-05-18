dc73adb4589ba5556f41c6792f6dbc800a6dc912
Gyeongtae Im <igt0530@gmail.com>
2022-05-18 23:05:30 +0000

    [keep] app-test.py

---
+++ part2/02-streamlit/app-test.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+# SETTING PAGE CONFIG TO WIDE MODE
+st.set_page_config(layout = "wide")
+
+st.write("Hello World")
+
+# TODO: Streamlit APP 만들기
+# TODO: Volia 코드 리팩토링(app.py, model.py, predict.py, utils.py)
+# TODO: File Uploader 구현
+# TODO: 이미지 View
+# TODO: 암호 입력
