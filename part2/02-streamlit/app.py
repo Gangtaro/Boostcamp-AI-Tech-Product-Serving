7be901f2abf7605d193885316c8df50a0a184a3f
Gyeongtae Im <45648852+Gangtaro@users.noreply.github.com>
2022-06-30 00:38:34 +0900

    Update app.py

--- part2/02-streamlit/app.py
+++ part2/02-streamlit/app.py
@@ -18,7 +18,7 @@ root_password = 'password'
 
 
 def main():
-    st.title("Mask Classification Model")
+    st.title("Mask Classification Model - Github Action Deploy [TEST - 01]")
 
     with open("config.yaml") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
